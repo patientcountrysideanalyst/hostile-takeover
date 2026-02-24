47e990835a2cef00ebc217e6e7636117f1365109
patientcountrysideanalyst <patientcountrysideanalyst@outlook.com>
2026-02-24 16:07:38 -0700

    Add Phase 1 Android Gradle modernization scaffold

---
+++ game/sdl/android/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.application") version "8.5.2"
+    kotlin("android") version "1.9.24"
+}
+
+android {
+    namespace = "com.spiffcode.ht"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.spiffcode.ht"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.63"
+    }
+
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("AndroidManifest.xml")
+            java.srcDirs("src")
+            res.srcDirs("res")
+            assets.srcDirs("assets")
+            jniLibs.srcDirs("libs")
+        }
+    }
+
+    externalNativeBuild {
+        ndkBuild {
+            path = file("jni/Android.mk")
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-project.txt"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    lint {
+        abortOnError = false
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
